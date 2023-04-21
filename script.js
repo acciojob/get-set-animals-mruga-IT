91e953c032d7773b0b30fa87f1110201adc3a4e3
mruga-IT <124659354+mruga-IT@users.noreply.github.com>
2023-04-21 11:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 //complete this code
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("The animal makes a sound");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
 class Animal {}
 
 class Dog extends Animal {}
